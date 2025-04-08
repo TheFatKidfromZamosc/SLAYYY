08bf22f8ef6e4d3be873ac9b04bab1c235c0364b
TheFatKidfromZamosc <witiuk61@gmail.com>
2025-04-08 13:44:24 +0200

    AAAA ANATONO NOO OTE

--- src/App.jsx
+++ src/App.jsx
@@ -5,86 +5,106 @@ import React, { useRef, useState } from "react";
 import { useForm } from 'react-hook-form';
 import { date, string, z } from "zod"; 
 import { zodResolver } from "@hookform/resolvers/zod"; 
-import f1 from './assets/img/f1.jpg';
-import f2 from './assets/img/f2.jpg';
-import c1 from './assets/img/c1.jpg';
-import c2 from './assets/img/c2.jpg';
-import z1 from './assets/img/z1.jpg';
-import z2 from './assets/img/z2.jpg';
-import fiolet from './assets/img/fioletowy.jpg';
-import czerwony from './assets/img/czerwony.jpg';
-import zloty from './assets/img/zloty.jpg';
-import wszystkie from './assets/img/wszystkie.jpg';
+
 // i nawet kiedy będe sam, nie zmienie się, sperme będe jadł
- const App = () => { 
 
-  const Lakiery= 
-    [
-      {plik : f1, odcień : "fioletowy", nazwa:"No Risk No story", cena : 27.29, reklama : "Oszczedzasz 33%" },
-      {plik : f2, odcień : "fioletowy", nazwa:"Moony Wispers", cena : 38.99, reklama : "Mój wybór Kasia Kot" },
-      {plik : c1, odcień : "czerwony", nazwa:"Alizee", cena : 27.29, reklama : "Oszczedzasz 31%" },
-      {plik : c2, odcień : "czerwony", nazwa:"Mysterious Tale", cena : 32.99, reklama : "GET THE LOOK" },
-      {plik : z1, odcień : "złoty", nazwa:"Top Glow Gold", cena : 29.99, reklama : "XMAS Trend" },
-      {plik : z2, odcień : "złoty", nazwa:"Sparkling Kiss", cena : 27.99, reklama : "XMAS Trend" },
-    ]
-    function dodajDoKoszyka(index){
-      alert(Lakiery[index].nazwa + " dodano do koszyka");
-    }  
+//  <GridGamer/>
 
-    function Wszyskie(){
-      return (
-       Lakiery.map((lak, index) => 
-          <div className='LakMain' key={index}>
-           <div className="imgDiv"> <img src={lak.plik} alt={lak.nazwa} /></div>
-           <div className="Opis">
-             <h4>{lak.nazwa}</h4>
-             <p>odcień : {lak.odcień}</p>
-             <p>{lak.cena}</p>
-             <p className='PODSWIETL'>{lak.reklama}</p>
-             <button onClick={ () =>dodajDoKoszyka(index)}>Dodaj Do Koszyka</button>
-             </div>  
-          </div>
-        )
-      );
-      }
-
- /*     function Fiolet(){
-        return (
-         Lakiery.map((lak, index) => {lak.odcień==="fioletowy" ?  <div className='LakMain' key={index}>
-         <div className="imgDiv"> <img src={lak.plik} alt={lak.nazwa} /></div>
-         <div className="Opis">
-           <h4>{lak.nazwa}</h4>
-           <p>odcień : {lak.odcień}</p>
-           <p>{lak.cena}</p>
-           <p className='PODSWIETL'>{lak.reklama}</p>
-           <button onClick={ () =>dodajDoKoszyka(index)}>Dodaj Do Koszyka</button>
-           </div>  
-        </div>)) : "" }  
-           
-          )
-        );
-        }
- */
-  
-    const [color, setColor] = useState("");
-  function Lakier(){
+const App = () => { 
+  const [marketList, setMarketList] = useState([]);
+  const [CombinedPrice, setCombinedPrice] = useState(0);
+  function Delete(index, price){
+    const updatedCart= marketList.filter((_,i) => i==index);
+    setMarketList(updatedCart);
+    setCombinedPrice(com => com-parseFloat(price))
+  }
+  function Wszystkie(){
     return(
       <>
-    <h1 className='head'>Lakiery</h1>
-      <div className='color'>
-          <img src={fiolet} alt="fioletowy" onClick={() => setColor("fioletowy")}/>
-          <img src={czerwony} alt="czerwony"  onClick={() => setColor("czerwony")}/>
-          <img src={zloty} alt="zloty"  onClick={() => setColor("zloty")}/>
-          <img src={wszystkie} alt="wszystkie"  onClick={() => setColor("wszystkie")}/>
+      <ol>
+      {marketList.map((ml, id) =><li key={id}>{ml.Name} - {ml.Price} - {ml.Category} <button onClick={() => Delete(id, ml.Price)}>DELETE </button> </li> )}
+      </ol>
+      </>
+     
+    );}
+    function Filtrowane(kat){
+      let filteredList;
+      filteredList = marketList.filter((marketList) => marketList.Category ===kat);
+      console.log(filteredList)
+      
+      return(
+        <ol>
+        {filteredList.map((ml, id) => <li key={id}>{ml.Name} - {ml.Price} - {ml.Category}</li>
+           )}
+         </ol>
+      );
+    }
+  
+  function GridGamer(){
+      const[kategoria, setKategoria] = useState("Wszystkie");
+      function onChange(e){
+        setKategoria(e.target.value);
+      }
+      return(
+      <>
+        Filtowanie 
+        <select onChange={onChange}>
+        <option  value="Wszystkie">Wszystkie</option>
+          <option value="Jedzenie">Jedzenie</option>
+          <option value="Elektronika">Elektronika</option>
+          <option value="Ubrania">Ubrania</option>
+          <option value="Dodatki">Dodatki</option>
+        </select> <br/>
+ {kategoria === "Wszystkie" ? <Wszystkie/>  : 
+Filtrowane(kategoria)
+ } 
+  <p>Całkowita cena : {CombinedPrice}</p>
+      </>
+    )
+  }
+  function UserForm() { 
+    let NameRef = useRef(null);
+    let KwotaRef = useRef(null);
+    let KategoriaRef = useRef(null);
+   
+   function handleOnClick(){
+   setCombinedPrice(com =>  com +parseFloat(KwotaRef.current.value))
+    setMarketList(ml => [...ml,{ Name : NameRef.current.value, 
+        Price : KwotaRef.current.value,
+         Category : KategoriaRef.current.value}]);
+       
+        }
+    return (
+    <form  onSubmit={handleOnClick}> 
+      <div className="mb-3"> 
+        <label htmlFor="Nazwa" className="form-label">Nazwa</label> 
+        <input type="text" id="Nazwa" className="form-control" ref={NameRef}  required /> 
       </div>
+
+      <div className="mb-3"> 
+        <label htmlFor="Kwota" className="form-label">Kwota</label> 
+        <input type="number" id="Kwota" className="form-control"  ref={KwotaRef} required />
+         
+      </div>
+
+      <select name="product" id="product" ref={KategoriaRef}  required>
+          <option value="Jedzenie">Jedzenie</option>
+          <option value="Elektronika">Elektronika</option>
+          <option value="Ubrania">Ubrania</option>
+          <option value="Dodatki">Dodatki</option>
+        </select> <br/>
+      <button type="submit" className="btn btn-primary" >Wyślij</button> 
+    </form> 
+  ); 
+}
+
     
-    <div>
-      <Wszyskie/>
-    </div>
-    </>);}
+
+
   return(
     <>
-     <Lakier/>
+  <UserForm/>
+  <GridGamer/>
     </>
   
   );
